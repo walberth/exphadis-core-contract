6c5851a76caa28cb19016a5cadf37b803e1132a3
wgutierrez <w.felipe.gutierrez@gmail.com>
2025-12-04 23:57:47 -0500

    feat: Add WhatsappSendRequestDto class for sending WhatsApp messages with required properties

---
+++ Dto/WhatsappSendRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Exphadis.Core.Contract.Dto
+{
+    public class WhatsappSendRequestDto
+    {
+        public required string To { get; set; }
+        public required string Template { get; set; }
+        public required Dictionary<string, string> Parameters { get; set; }
+    }
+}
