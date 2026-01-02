9d4892687dda40a5ca8646d97093e74a3975ea2a
wgutierrez <w.felipe.gutierrez@gmail.com>
2026-01-02 01:23:14 -0500

    feat: Add EnrichRequestDto class with properties for user enrichment requests

---
+++ Dto/EnrichRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Exphadis.Core.Contract.Dto
+{
+    public class EnrichRequestDto
+    {
+        public string Username { get; set; }
+        public string Location { get; set; }
+        public string CompleteName { get; set; }
+        public string AppClientId { get; set; }
+        public string SentryId { get; set; }
+        public string QueryString { get; set; }
+        public string RouteParams { get; set; }
+        public string RequestBody { get; set; }
+        public string TraceIdentifier { get; set; }
+    }
+}
