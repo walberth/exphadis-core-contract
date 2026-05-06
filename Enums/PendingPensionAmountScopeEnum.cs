48e911bb1ca502c0be294795d4a97601000b299d
wgutierrez <w.felipe.gutierrez@gmail.com>
2026-05-06 00:25:49 -0500

    feat: Add PendingPensionAmountScopeEnum for pension amount scope management

---
+++ Enums/PendingPensionAmountScopeEnum.cs
@@ -0,0 +1,10 @@
+namespace Exphadis.Core.Contract.Enums;
+
+public enum PendingPensionAmountScopeEnum
+{
+    [EnumMember(Value = "ACCUMULATED")]
+    Accumulated = 0,
+
+    [EnumMember(Value = "EXACT")]
+    Exact = 1,
+}
