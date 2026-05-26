7268e94f1d2c17c3b38c6139d92cc033a981551c
wgutierrez <w.felipe.gutierrez@gmail.com>
2026-05-26 10:31:01 -0500

    feat: Añadir propiedad RadicaleHref al DTO RadicaleContactDto

--- Dto/RadicaleContactDto.cs
+++ Dto/RadicaleContactDto.cs
@@ -4,6 +4,8 @@ namespace Exphadis.Core.Contract.Dto
     {
         public string Id { get; set; } = string.Empty;
 
+        public string RadicaleHref { get; set; } = string.Empty;
+
         public string DisplayName { get; set; } = string.Empty;
 
         public string PhoneNumber { get; set; } = string.Empty;
